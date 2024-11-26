412a93fa00e38e05464b9a14058089ffe5ae146a
Tony <srnipoi.163041.10@gmail.com>
2024-11-26 21:43:26 +0000

    add new feature

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World! Anthony? You Well?'));
+app.get('/', (req, res) => res.send('Hello World! Anthony? '));
+
+// Динамическое приветствие
+app.get('/greet/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello, ${name}! Welcome to our site.`);
+});
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
